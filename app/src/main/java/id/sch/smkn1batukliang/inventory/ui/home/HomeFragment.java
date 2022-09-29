55cdc33342311eb288449e1e3dcac6995c869cbe
Muhamad Aprizal Hardi <aprizalrizalzal@gmail.com>
2022-09-29 11:59:07 +0800

    Inventory Project update home web view true javaScript

--- app/src/main/java/id/sch/smkn1batukliang/inventory/ui/home/HomeFragment.java
+++ app/src/main/java/id/sch/smkn1batukliang/inventory/ui/home/HomeFragment.java
@@ -1,7 +1,9 @@
 package id.sch.smkn1batukliang.inventory.ui.home;
 
+import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,9 +19,6 @@ import id.sch.smkn1batukliang.inventory.utili.CustomProgressDialog;
 
 public class HomeFragment extends Fragment {
 
-    private View view;
-    private CustomProgressDialog progressDialog;
-
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -29,18 +28,39 @@ public class HomeFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         FragmentHomeBinding binding = FragmentHomeBinding.inflate(inflater, container, false);
-        view = binding.getRoot();
+        View view = binding.getRoot();
 
-        progressDialog = new CustomProgressDialog(requireActivity());
+        CustomProgressDialog progressDialog = new CustomProgressDialog(requireActivity());
         String url = "https://smkn1batukliang.sch.id/";
 
         WebView webView = binding.webView;
         webView.setWebViewClient(new MyWebViewClient(progressDialog));
+        webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl(url);
+
+        webView.setOnKeyListener((v, keyCode, event) -> {
+            if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                if (keyCode == KeyEvent.KEYCODE_BACK) {
+                    if (webView.canGoBack()) {
+                        webView.goBack();
+                    } else {
+                        requireActivity().finish();
+                    }
+                    return true;
+                }
+            }
+            return false;
+        });
+
+        binding.refreshLayout.setOnRefreshListener(() -> {
+            webView.loadUrl(url);
+            binding.refreshLayout.setRefreshing(false);
+        });
 
         return view;
     }
